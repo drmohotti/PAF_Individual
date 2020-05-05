38bb7940011a28e03ef1e4bdcacad38acc9bab99
drmohotti <drmohotti@gmail.com>
2020-05-05 13:34:32 +0530

    10.8 Map

--- helthcare_system/src/model/VisitsAPI.java
+++ helthcare_system/src/model/VisitsAPI.java
@@ -16,52 +16,77 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/VisitsAPI")
 public class VisitsAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public VisitsAPI() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public VisitsAPI() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//doGet(request, response);
-		
+		// doGet(request, response);
+
 		Visit visitObj = new Visit();
 		String output = visitObj.insertVisits(request.getParameter("hospital_name"),
-				request.getParameter("hospital_city"),
-				request.getParameter("date"),
-				request.getParameter("time"),
+				request.getParameter("hospital_city"), request.getParameter("date"), request.getParameter("time"),
 				request.getParameter("noPatients"));
-				
-				response.getWriter().write(output);
+
+		response.getWriter().write(output);
 	}
 
 	/**
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
 	 */
-	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
+	private static Map getParasMap(HttpServletRequest request)
+	{
+		Map<String, String> map = new HashMap<String, String>();
+		try
+		{
+			Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+			String queryString = scanner.hasNext() ? scanner.useDelimiter("\\A").next() : "";
+			scanner.close();
+			
+			String[] params = queryString.split("&");
+			for (String param : params)
+			{ 	
+				String[] p = param.split("=");
+				map.put(p[0], p[1]);
+			}
+	}
+	catch (Exception e)
+	{
+	}
+	return map;
+	}
 }
